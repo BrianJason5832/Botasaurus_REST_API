c5b41ae024401bafcb9b9ba45cbf9c31a56f40fc
BryanMunyua <bryanmunyua@gmail.com>
2025-04-22 00:54:02 +0300

    Initial commit

--- backend/scrapers.py
+++ backend/scrapers.py
@@ -3,3 +3,6 @@ from src.scrape_heading_task import scrape_heading_task
 
 # Add the scraper to the server
 Server.add_scraper(scrape_heading_task)
+
+# Connect to PostgreSQL server
+Server.set_database_url('postgresql://postgres.updemyybuhoicisjksiu:Dexter_#254@aws-0-eu-central-1.pooler.supabase.com:6543/postgres')
